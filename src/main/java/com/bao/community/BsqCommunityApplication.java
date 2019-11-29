1b120582431df6df887b7a7d74f43ec4152a1134
baosq <634269333@qq.com>
2019-11-30 00:16:24 +0800

    init repo

---
+++ src/main/java/com/bao/community/BsqCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.bao.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsqCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BsqCommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bao/community/BsqCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bao.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsqCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
