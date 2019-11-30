7cf1b084096e40b31f600861fa07bc2ed1081df8
baosq <634269333@qq.com>
2019-11-30 18:27:51 +0800

    add index and bootstrap

---
+++ src/main/java/com/bao/community/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.bao.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @auther Bao
+ * @date 2019/11/30 15:26
+ */
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+        return "index.html";
+    }
+}
